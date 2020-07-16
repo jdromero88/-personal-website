1d6568c59a6eccdd4f0529194083c9094a5461b7
jdromero88 <jdromerov88@gmail.com>
2020-07-16 01:35:17 -0400

    Render resume in Resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import {Container, Header, Grid,} from 'semantic-ui-react'
+import { Document } from 'react-pdf/dist/entry.webpack';
+import { Page } from 'react-pdf'
+import JRResume from '../assets/Jose Romero - Resume.pdf'
 
 const Resume = () => {
   return(
     <Container fluid>
       <Grid stackable>
         <Grid.Row>
-          <Grid.Column width={16}>
+          <Grid.Column width={4}>
           </Grid.Column>
+          <Grid.Column width={8}>
+            <Header className='header' as='h1'>Résumé</Header>
+            <Document file={JRResume}>
+              <Page pageNumber={1} />
+            </Document>
+          </Grid.Column>
+          <Grid.Column width={4}>
+          </Grid.Column>            
         </Grid.Row>
       </Grid>
     </Container>
