9926b6604f14ac93c6281f17ccc3e066c2ec463d
jdromero88 <jdromerov88@gmail.com>
2020-07-16 02:03:52 -0400

    Removed from master the portal transition on Skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import { Grid, Accordion, Icon,
-  Button, Header, Segment, TransitionablePortal} from 'semantic-ui-react'
+import { Grid, Accordion, Icon } from 'semantic-ui-react'
 
 class Skills extends React.Component {
-  // state = { activeIndex: 0,
-  //   open: false,
-  //  }
-  state = { open: true,
+  state = { activeIndex: 0,
    }
 
   handleClick = (e, titleProps) => {
     const { index } = titleProps
     const { activeIndex } = this.state
     const newIndex = activeIndex === index ? -1 : index
-    this.setState({ activeIndex: newIndex, open: !this.state.open })
+    this.setState({ activeIndex: newIndex})
   }
-  handleClose = () => this.setState({ open: false })
 
   render(){
     const { activeIndex } = this.state
@@ -23,26 +18,6 @@ class Skills extends React.Component {
       <Grid verticalAlign='middle' centered>
         <Grid.Row>
           <Grid.Column>
-            <Button
-              content={this.state.open ? 'Hide Skills' : 'Show Skills'}
-              negative={this.state.open}
-              positive={!this.state.open}
-              onClick={this.handleClick}
-            />
-            <TransitionablePortal onClose={this.handleClose}
-              open={this.state.open}>
-              <Segment
-                style={{ left: '40%', position: 'fixed', top: '50%', zIndex: 1000 }}
-              >
-                <Header>Technical Skills</Header>
-                  <p>
-                    Ruby, Ruby on Rails, JavaScript(vanilla)(ES6), ReactJS, Redux,
-                    HTML5, CSS, PostgreSQL, sqlLite, MVC, RESTful architecture,
-                  </p>
-                  <p>Linux, APIs, Wordpress.</p>
-                  <p>Bootstrap, Semantic UI, Bulma.</p>
-              </Segment>
-            </TransitionablePortal>
             <Accordion fluid styled>
               <Accordion.Title
                 active={activeIndex === 0}
