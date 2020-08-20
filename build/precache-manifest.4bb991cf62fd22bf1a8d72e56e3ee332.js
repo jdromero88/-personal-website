2ef32b2b64c758cd0af5e39fb2255d95b13832d0
jdromero88 <jdromerov88@gmail.com>
2020-08-19 21:46:35 -0400

    updated meta tags

---
+++ build/precache-manifest.4bb991cf62fd22bf1a8d72e56e3ee332.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a840cba14fd6fdf29fce64d71f7e80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89d81e78539e56f3328b",
+    "url": "/static/css/2.04716422.chunk.css"
+  },
+  {
+    "revision": "e39952254e720160dabb",
+    "url": "/static/css/main.d0e68908.chunk.css"
+  },
+  {
+    "revision": "89d81e78539e56f3328b",
+    "url": "/static/js/2.bca118f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bca118f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39952254e720160dabb",
+    "url": "/static/js/main.4e38183c.chunk.js"
+  },
+  {
+    "revision": "09f8e1a4fadd17dad6a5",
+    "url": "/static/js/runtime-main.0626e6de.js"
+  },
+  {
+    "revision": "cd3321efcbadd7c592a6f3f9bad123c3",
+    "url": "/static/media/Jose-Romero-resume.cd3321ef.pdf"
+  },
+  {
+    "revision": "d5d94b23e96fc242949854b332bd3bad",
+    "url": "/static/media/JoseRomero800.d5d94b23.jpg"
+  },
+  {
+    "revision": "34287a6f242e644b28d8344a270f7267",
+    "url": "/static/media/bromble.34287a6f.jpg"
+  },
+  {
+    "revision": "ed6bd6eefa7a682a2d2ef7b3d7664482",
+    "url": "/static/media/flatilife.ed6bd6ee.jpg"
+  },
+  {
+    "revision": "f041898ed4bad4f8cea93660f8db30e5",
+    "url": "/static/media/joseromero.f041898e.jpg"
+  },
+  {
+    "revision": "41da7ebf17a81f097e75e815ffaf9acc",
+    "url": "/static/media/kandj.41da7ebf.jpg"
+  },
+  {
+    "revision": "24898ea1ab4da590dbbef6efb9d03c0b",
+    "url": "/static/media/mercury-products.24898ea1.jpg"
+  },
+  {
+    "revision": "00469d0930576e760ea912099e8e8e5e",
+    "url": "/static/media/vapor.00469d09.jpg"
+  }
+]);
