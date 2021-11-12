ed140021ce306f0981b17b8b133f4d6538db840f
jdromero88 <jdromerov88@gmail.com>
2021-11-11 20:17:02 -0500

    Downgraded react, update resume

--- src/components/Relocate.jsx
+++ src/components/Relocate.jsx
@@ -4,7 +4,7 @@ const Relocate = () => {
   return(
     <article className="message mt-2">
       <div className="message-header">
-        <p><i className="fas fa-map-marker-alt"></i> Washington DC | Available for Hire</p>
+        <p><i className="fas fa-map-marker-alt"></i> Washington DC</p>
       </div>
       <div className="message-body">
         Willing to relocate to places that would increment my <code>happiness++</code>.
