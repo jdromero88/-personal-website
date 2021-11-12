8001aec9838f137e9d372c2deda418163aa3ba50
jdromero88 <jdromerov88@gmail.com>
2021-11-12 00:39:34 -0500

    added new projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Relocate from './Relocate'
 import Carousel from '../containers/Carousel'
+import CarouselProjects from '../containers/CarouselProjects'
 // import joseRomero from '../assets/joseromero.jpg'
 const Portfolio = () => {
   return(
     <React.Fragment>
       <p className="title is-1 has-text-centered pt-4">Projects</p>
+      <CarouselProjects />
+      <p className="title is-1 has-text-centered mt-6 pt-6">Websites</p>
       <Carousel />
       <Relocate />
     </React.Fragment>
